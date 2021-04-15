395d02fac2d756a89d9d44770eb8370ac52efd31
Enrico-git <enrico.alberti.18@gmail.com>
2021-04-15 12:07:11 +0200

    Lab2-step3: created action for moving between homeFragment and tripDetailsFragment

--- Lab/Lab2/app/src/main/java/it/polito/mad/group08/carpooling/ui/home/HomeFragment.kt
+++ Lab/Lab2/app/src/main/java/it/polito/mad/group08/carpooling/ui/home/HomeFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import it.polito.mad.group08.carpooling.R
 
 class HomeFragment : Fragment() {
@@ -27,5 +29,14 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_tripDetailsFragment)
+        }
     }
 }
