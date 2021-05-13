f7e39aea2c659385a1c4d90ba080d5dd23fb59c5
Enrico Alberti <enrico.alberti.18@gmail.com>
2021-05-13 12:03:43 +0200

    Interactive gesture on clock

---
+++ 2021-04-21/app/src/main/java/it/polito/s279434/clockview/MainActivity.kt
@@ -0,0 +1,21 @@
+package it.polito.s279434.clockview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clock = findViewById<ClockView>(R.id.clockView)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            clock.m = clock.m+1
+            if(clock.m==0)
+                clock.h = clock.h+1
+        }
+    }
+}
